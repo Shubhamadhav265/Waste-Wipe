78eae7627c8cbe62d39a9b08a18568304347aa32
Shubham Adhav <116251932+Shubhamadhav265@users.noreply.github.com>
2023-11-24 19:51:11 +0530

    Add files via upload

---
+++ Hmpg.js
@@ -0,0 +1,54 @@
+// $(document).ready(() => {
+//     $('#nav-toggle').click(() => {
+//       $(".nav-item").toggle(100);
+//       $(".nav-link").toggle(100);
+//     });
+//  });
+
+
+
+const myFunction = () => {
+  document.getElementById("first").style.display = 'block';
+  document.getElementById("second").style.display = 'none';
+  document.getElementById("third").style.display = 'none';
+  document.getElementById("fourth").style.display = 'none';
+  document.getElementById("fifth").style.display = 'none';
+};
+
+const myFunction2 = () => {
+  document.getElementById("first").style.display = 'none';
+  document.getElementById("second").style.display = 'block';
+  document.getElementById("third").style.display = 'none';
+  document.getElementById("fourth").style.display = 'none';
+  document.getElementById("fifth").style.display = 'none';
+};
+
+const myFunction3 = () => {
+  document.getElementById("first").style.display = 'none';
+  document.getElementById("second").style.display = 'none';
+  document.getElementById("third").style.display = 'block';
+  document.getElementById("fourth").style.display = 'none';
+  document.getElementById("fifth").style.display = 'none';
+};
+
+const myFunction4 = () => {
+  document.getElementById("first").style.display = 'none';
+  document.getElementById("second").style.display = 'none';
+  document.getElementById("third").style.display = 'none';
+  document.getElementById("fourth").style.display = 'block';
+  document.getElementById("fifth").style.display = 'none';
+};
+
+const myFunction5 = () => {
+  document.getElementById("first").style.display = 'none';
+  document.getElementById("second").style.display = 'none';
+  document.getElementById("third").style.display = 'none';
+  document.getElementById("fourth").style.display = 'none';
+  document.getElementById("fifth").style.display = 'block';
+};
+
+// const button = document.getElementById("myButton");
+
+// button.addEventListener("click", function() {
+//   button.classList.add("clicked");
+// });
